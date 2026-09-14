670658cd713cd042092a6cd36ef667eb024041de
Timofey Gridin <timasgridin@gmail.com>
2026-09-14 11:31:26 +0300

    Fix part 16 task 3

--- homework2/Number16/Task3.py
+++ homework2/Number16/Task3.py
@@ -7,6 +7,8 @@ def min_coins(n, coins):
     dp[0] = 0
     for i in range(1, n + 1):
         for c in coins:
+            if i < c:
+                continue
             dp[i] = min(dp[i], dp[i - c] + 1)
 
     if dp[n] > n:
